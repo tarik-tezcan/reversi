71bbe6b43e3e87b66ae92531bed961223dcd8e5d
avant-gardener <tariktezcan2001@gmail.com>
2022-01-29 02:27:51 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <Reversi />
+</template>
+
+<script>
+import Reversi from './components/Reversi.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Reversi,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/components/Reversi.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="grid">
+    <div class="grid-row" v-for="item in gridSize" :key="item.id">
+      <div
+        class="square"
+        :style="{ width: cellWidth + 'px', height: cellWidth + 'px' }"
+        v-for="item2 in gridSize"
+        :key="item2.id"
+      >
+        {{ (item - 1) * gridSize + item2 }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ReversiGrid',
+  data() {
+    return {
+      gridSize: 8,
+      cellWidth: 64,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#grid {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: black;
+  max-width: fit-content;
+  border: 2px solid black;
+}
+
+.grid-row {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+
+.square {
+  border: 2px solid black;
+  background-color: lightgray;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
